07e4a9bd478449b6b5c7ca97c743a89eb1f76ce0
Ilan Germano <ige22@uclive.ac.nz>
2026-02-25 18:08:30 +1300

    Added lab1 ex 5 and 6

---
+++ Week-1/lab1_ex6/app.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('HTTP request: GET /');
+});
+
+app.post('/', (req, res) => {
+    res.send('HTTP request: POST /');
+});
+
+app.put('/:id', (req, res) => {
+    res.send('HTTP request: PUT /');
+});
+
+app.delete('/:id', (req, res) => {
+    res.send('HTTP request: DELETE /');
+});
+
+app.listen(3000, () => {
+    console.log("App listening on port 3000");
+});
+
