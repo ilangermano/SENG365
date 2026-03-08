b528ae3f2aed3c0e8cddf5a5e583049f1797577c
Ilan Germano <ige22@uclive.ac.nz>
2026-03-09 12:12:14 +1300

    Feat: added features to server controller and server models. Remember to add .env on pull.

--- Week-2/lab_2/src/app/controllers/user.server.controller.ts
+++ Week-2/lab_2/src/app/controllers/user.server.controller.ts
@@ -9,17 +9,39 @@ const list = async (req: Request, res: Response): Promise<void> => {
         res.status(200).send(result);
     } catch (err) {
         res.status(500)
-            .send(`Error getting users ${err}`);
+            .send(`ERROR getting users ${err}`);
     }
 };
 
 const create = async (req: Request, res: Response): Promise<void> => {
-    return null;
-}
+    Logger.http(`POST create a user with username: ${req.body.username}`)
+    if (! req.body.hasOwnProperty("username")){
+        res.status(400).send('Please provide a username field');
+        return;
+    }
+    const username = req.body.username;
+    try {
+        const insertId = await users.insert( username );
+        res.status(201).send({"user_id": insertId});
+    } catch (err) {
+        res.status(500).send(`ERROR creating user ${username}: ${err}`);
+    }
+};
 
 const read = async (req: Request, res: Response): Promise<void> => {
-    return null;
-}
+    Logger.http(`GET single user id: ${req.params.id}`)
+    const id = typeof req.params.id === 'string' ? req.params.id : req.params.id[0];
+    try {
+        const result = await users.getOne(parseInt(id, 10));
+        if ( result.length === 0 ) {
+            res.status(404 ).send('User not found.');
+        } else {
+            res.status(200).send(result[0]);
+        }
+    } catch (err) {
+        res.status(500).send(`ERROR getting user ${id}: ${err}`);
+    }
+};
 
 const update = async (req: Request, res: Response): Promise<void> => {
     return null;
--- Week-2/lab_2/src/app/models/user.server.models.ts
+++ Week-2/lab_2/src/app/models/user.server.models.ts
@@ -14,13 +14,27 @@ const getAll = async (): Promise<User[]> => {
     }));
 };
 
-const getOne = async (): Promise<any> => {
-    return null;
-}
+const getOne = async (id: number): Promise<User[]> => {
+    Logger.info(`Getting user ${id} from the database`);
+    const conn = await getPool().getConnection();
+    const query = 'select * from lab2_users where user_id = ?';
+    const [ rows ] = await conn.query<RowDataPacket[]>( query, [ id ]);
+    conn.release();
+    return rows.map(row => ({
+        user_id: row.user_id,
+        username: row.username,
+        email: row.email,
+    }));
+};
 
-const insert = async (): Promise<any> => {
-    return null;
-}
+const insert = async (username: string): Promise<number> => {
+    Logger.info(`Adding user ${username} to the database`);
+    const conn = await getPool().getConnection();
+    const query = 'insert into lab2_users (username) values ( ? )';
+    const [ result ] = await conn.query<ResultSetHeader>( query, [ username ] );
+    conn.release();
+    return result.insertId;
+};
 
 const alter = async (): Promise<any> => {
     return null;
